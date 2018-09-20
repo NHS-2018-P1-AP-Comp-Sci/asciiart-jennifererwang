93a9caac597d8ff142ba61087c93cff49333ddc0
s-wangxu <s-wangxu@53S50030918.it.bsd405.org>
2018-09-20 08:21:56 -0700

    Unit 1 Project

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -15,3 +15,61 @@
  */
 
 // YOUR CODE GOES HERE :)
+
+public class ASCIIArt {
+	public static void main(String args[]) {
+		drawQuote();
+		drawEars();
+		drawEars();
+		System.out.println("\"-----\"");
+		drawEyes();
+		drawMouth();
+		System.out.println("-------");
+		drawHands();
+		drawFeet();
+		System.out.println("\nThis is a little creature called Bob. \nHe has two LONG ears, two eyes, two hands and two feet.");
+		System.out.println("\nNote: the blank spaces in between the ears may show up as blue lines when you run this code.");
+	}
+	public static void drawEars() {
+		System.out.println("\"" + "     " + "\"");
+	}
+	public static void drawEyes() {
+		System.out.println("| > < |");
+	}
+	public static void drawMouth() {
+		System.out.println("|  -  |");
+	}
+	public static void drawHands() {
+		System.out.println(" -| |- ");
+	}
+	public static void drawFeet() {
+		System.out.println("  / \\  ");
+		System.out.println(" /   \\  ");
+	}
+	public static void drawQuote() {
+		System.out.println("\t __________");
+		System.out.println("\t|          |");
+		System.out.println("\t| Hello!!  |");
+		System.out.println("\t| I'm Bob. |");
+		System.out.println("\t|__________|");
+		System.out.println("\t //");
+		System.out.println("\t//");
+	}
+}
+
+/*
+		 __________
+		| Hello!!  |
+		|__________|
+		 //
+		//
+"     "
+"	  "
+"-----"
+| > < |
+|  -  |
+-------
+ -| |- 
+  / \
+ /   \
+   */
